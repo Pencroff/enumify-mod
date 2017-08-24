c4954e1104cde7ea597e2180a564c4d8ead518e1
Sergey Daniloff (@Pencroff) <pencroff@gmail.com>
2017-08-24 22:48:14 +0100

    feat(Mod for Enumify): Implementation proposal and tests
    Enumify modification with `valueOf`, `fromValue`, `fromName`

--- src/enumify-mod.ts
+++ src/enumify-mod.ts
@@ -1,4 +1,151 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-// import "core-js/fn/array.find"
-// ...
-export default class DummyClass {}
+/**
+ * Created by Sergey Daniloff on 23-Aug-2017.
+ * Based on enumify idea - https://github.com/rauschma/enumify and proposal http://goo.gl/XCh8Lc
+ */
+
+const INITIALIZED = Symbol()
+
+export type EnumArgType = any[] | object
+
+/**
+ * This is an abstract class that is not intended to be
+ * used directly. Extend it to turn your class into an enum
+ * (initialization is performed via `MyClass.initEnum()`).
+ */
+export class Enum {
+  public static enumValues: any
+  public name: string
+
+  /**
+   * `initEnum()` closes the class. Then calling this constructor
+   * throws an exception.
+   *
+   * If your subclass has a constructor then you can control
+   * what properties are added to `this` via the argument you
+   * pass to `super()`. No arguments are fine, too.
+   */
+  constructor(instanceProperties: object) {
+    // new.target would be better than this.constructor,
+    // but isn’t supported by Babel
+    if ({}.hasOwnProperty.call(this.constructor, INITIALIZED)) {
+      throw new Error("Enum classes can’t be instantiated")
+    }
+    if (typeof instanceProperties === "object" && instanceProperties !== null) {
+      copyProperties(this, instanceProperties)
+    }
+  }
+
+  /**
+   * Create new Enum by name and description
+   * @param {string} enumName, enum name, used in `toString`
+   * @param {EnumArgType} arg Describing new fields of enum
+   * @returns {any}
+   */
+  public static create(enumName: string, arg: EnumArgType): any {
+    const newEnum = class extends Enum {}
+    newEnum.prototype.constructor = new Function(
+      `return function ${enumName}() {}`
+    )()
+    return newEnum.initEnum(arg)
+  }
+  /**
+   * Set up the enum, close the class.
+   *
+   * @param arg Either an object whose properties provide the names
+   * and values (which must be mutable objects) of the enum constants.
+   * Or an Array whose elements are used as the names of the enum constants
+   * The values are create by instantiating the current class.
+   */
+  public static initEnum(arg: EnumArgType) {
+    Object.defineProperty(this, "enumValues", {
+      value: [],
+      configurable: false,
+      writable: false,
+      enumerable: true
+    })
+    if (Array.isArray(arg)) {
+      this._enumValuesFromArray(arg, this) // Typescript not support ES6 version
+    } else {
+      this._enumValuesFromObject(arg)
+    }
+    Object.freeze(this.enumValues)
+    let ctx: any = this
+    ctx[INITIALIZED] = true
+    return ctx
+  }
+
+  /**
+   * Get Enum instance by value
+   * @param value
+   */
+  public static fromValue(value: any) {
+    return this.enumValues.find((x: any) => x.value === value)
+  }
+
+  /**
+   * Get Enum instance by name
+   * @param {string} name
+   */
+  public static fromName(name: string) {
+    return this.enumValues.find((x: any) => x.name === name)
+  }
+
+  /**
+   * Make enum classes iterable
+   */
+  public static [Symbol.iterator]() {
+    return this.enumValues[Symbol.iterator]()
+  }
+
+  private static _enumValuesFromArray(arr: any[], context: any) {
+    for (const key of arr) {
+      this._pushEnumValue(new context(), key)
+    }
+  }
+
+  private static _enumValuesFromObject(obj: any) {
+    Object.keys(obj).forEach((key: string) => {
+      let keyValue = obj[key]
+      if (typeof keyValue !== "object") {
+        keyValue = {
+          value: obj[key]
+        }
+      }
+      const value = new this(keyValue)
+      this._pushEnumValue(value, key)
+    })
+  }
+
+  private static _pushEnumValue(enumValue: any, name: string) {
+    enumValue.name = name
+    if (typeof enumValue.value === "undefined") {
+      enumValue.value = this.enumValues.length
+    }
+    enumValue.valueOf = function valueOf() {
+      return this.value
+    }
+    Object.defineProperty(this, name, {
+      value: enumValue,
+      configurable: false,
+      writable: false,
+      enumerable: true
+    })
+    this.enumValues.push(enumValue)
+  }
+
+  /**
+   * Default `toString()` method for enum constant.
+   */
+  public toString() {
+    return `${this.constructor.name}.${this.name}`
+  }
+}
+function copyProperties(target: object, source: object) {
+  // Ideally, we’d use Reflect.ownKeys() here,
+  // but I don’t want to depend on a polyfill
+  for (const key of Object.getOwnPropertyNames(source)) {
+    const desc = Object.getOwnPropertyDescriptor(source, key)
+    Object.defineProperty(target, key, desc)
+  }
+  return target
+}
--- test/enumify-mod.test.ts
+++ test/enumify-mod.test.ts
@@ -1,14 +1,265 @@
-import DummyClass from "../src/enumify-mod"
+import { Enum } from "../src/enumify-mod"
 
-/**
- * Dummy test
- */
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
-    expect(true).toBeTruthy()
+describe("Enum test", () => {
+  describe("Enum: instantiation", () => {
+    it('should instantiate Enum by "extends"', done => {
+      class Color extends Enum {
+        static RED: Color
+        static GREEN: Color
+        static BLUE: Color
+      }
+      Color.initEnum(["RED", "GREEN", "BLUE"])
+      expect(Color.RED).toBeInstanceOf(Enum)
+      expect(Color.RED).toBeInstanceOf(Color)
+      done()
+    })
+    it('should instantiate Enum by "create"', done => {
+      const Color = Enum.create("Color", ["RED", "GREEN", "BLUE"])
+      expect(Color.RED).toBeInstanceOf(Enum)
+      expect(Color.RED).toBeInstanceOf(Color)
+      done()
+    })
   })
-
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  describe("Enum: simple", () => {
+    let Color
+    beforeEach(done => {
+      Color = Enum.create("Color", ["RED", "GREEN", "BLUE"])
+      done()
+    })
+    it("instanceof", done => {
+      expect(Color.RED).toBeInstanceOf(Enum)
+      expect(Color.RED).toBeInstanceOf(Color)
+      done()
+    })
+    it("toString", () => {
+      expect(String(Color.RED)).toEqual("Color.RED")
+      expect(Color.RED.toString()).toEqual("Color.RED")
+    })
+    it('should return ordinal value in "valueOf" call by default', done => {
+      expect(Color.RED.valueOf()).toEqual(0)
+      expect(Color.GREEN.valueOf()).toEqual(1)
+      done()
+    })
+    it('should return declared value in "valueOf" call', done => {
+      Color = Enum.create("Color", {
+        RED: "#F00",
+        GREEN: "#0F0",
+        BLUE: "#00F"
+      })
+      expect(Color.RED.valueOf()).toEqual("#F00")
+      expect(Color.GREEN.valueOf()).toEqual("#0F0")
+      done()
+    })
+    it('should return value from attached objects in "valueOf" call', done => {
+      Color = Enum.create("Color", {
+        RED: {
+          value: "#F00",
+          alias: "red"
+        },
+        GREEN: {
+          value: "#0F0",
+          alias: "green"
+        },
+        BLUE: {
+          value: "#00F",
+          alias: "blue"
+        }
+      })
+      expect(Color.RED.valueOf()).toEqual("#F00")
+      expect(Color.GREEN.valueOf()).toEqual("#0F0")
+      done()
+    })
+    it("should keep additional fields from attached objects", done => {
+      Color = Enum.create("Color", {
+        RED: {
+          value: "#F00",
+          alias: "red"
+        },
+        GREEN: {
+          value: "#0F0",
+          alias: "green"
+        },
+        BLUE: {
+          value: "#00F",
+          alias: "blue"
+        }
+      })
+      expect(Color.RED.valueOf()).toEqual("#F00")
+      expect(Color.RED.alias).toEqual("red")
+      expect(Color.GREEN.valueOf()).toEqual("#0F0")
+      expect(Color.GREEN.alias).toEqual("green")
+      done()
+    })
+    it("should apply ordinal for attached objects", done => {
+      Color = Enum.create("Color", {
+        RED: {
+          alias: "red"
+        },
+        GREEN: {
+          alias: "green"
+        },
+        BLUE: {
+          alias: "blue"
+        }
+      })
+      expect(Color.RED.valueOf()).toEqual(0)
+      expect(Color.RED.alias).toEqual("red")
+      expect(Color.GREEN.valueOf()).toEqual(1)
+      expect(Color.GREEN.alias).toEqual("green")
+      done()
+    })
+    it("should be not equal to declared value", done => {
+      Color = Enum.create("Color", {
+        RED: "#F00",
+        GREEN: "#0F0",
+        BLUE: "#00F"
+      })
+      expect(Color.RED === "#F00").toEqual(false)
+      expect(Color.GREEN === "#0F0").toEqual(false)
+      done()
+    })
+    it('should implement "fromValue" method', done => {
+      Color = Enum.create("Color", {
+        RED: "#F00",
+        GREEN: "#0F0",
+        BLUE: {
+          alias: "blue"
+        }
+      })
+      expect(Color.fromValue("#0F0")).toEqual(Color.GREEN)
+      expect(Color.fromValue(2)).toEqual(Color.BLUE)
+      done()
+    })
+    it('should implement "fromName" method', done => {
+      Color = Enum.create("Color", {
+        RED: "#F00",
+        GREEN: "#0F0",
+        BLUE: "#00F"
+      })
+      expect(Color.fromName("GREEN")).toEqual(Color.GREEN)
+      expect(Color.fromName("BLUE")).toEqual(Color.BLUE)
+      done()
+    })
+    it('should support "enumValues"', done => {
+      expect(Color.enumValues).toEqual([Color.RED, Color.GREEN, Color.BLUE])
+      done()
+    })
+  })
+  describe("Enum: custom constructor and instance method", () => {
+    // Alias, data properties don’t work, because the Enum
+    // values (TicTacToe.X etc.) don’t exist when
+    // the object literals are evaluated.
+    const TicTacToe = Enum.create("TicTacToe", {
+      O: {
+        get inverse() {
+          return (TicTacToe as any).X
+        }
+      },
+      X: {
+        get inverse() {
+          return (TicTacToe as any).O
+        }
+      }
+    })
+    it("should support custom instance property", done => {
+      expect(TicTacToe.X.inverse).toEqual(TicTacToe.O)
+      expect(TicTacToe.O.inverse).toEqual(TicTacToe.X)
+      done()
+    })
+    it("toString", () => {
+      expect(String(TicTacToe.O)).toEqual("TicTacToe.O")
+    })
+    it("valueOf", () => {
+      expect(TicTacToe.O.valueOf()).toEqual(0)
+      expect(TicTacToe.X.valueOf()).toEqual(1)
+    })
+  })
+  describe("Enum: custom prototype method", () => {
+    class Weekday extends Enum {
+      static MONDAY
+      static TUESDAY
+      static WEDNESDAY
+      static THURSDAY
+      static FRIDAY
+      static SATURDAY
+      static SUNDAY
+      isBusinessDay() {
+        switch (this) {
+          case Weekday.SATURDAY:
+          case Weekday.SUNDAY:
+            return false
+          default:
+            return true
+        }
+      }
+    }
+    Weekday.initEnum([
+      "MONDAY",
+      "TUESDAY",
+      "WEDNESDAY",
+      "THURSDAY",
+      "FRIDAY",
+      "SATURDAY",
+      "SUNDAY"
+    ])
+    it("Custom prototype method", () => {
+      expect(Weekday.SATURDAY.isBusinessDay()).toEqual(false)
+      expect(Weekday.MONDAY.isBusinessDay()).toEqual(true)
+    })
+    it('should support "enumValues"', () => {
+      expect(Weekday.enumValues).toEqual([
+        Weekday.MONDAY,
+        Weekday.TUESDAY,
+        Weekday.WEDNESDAY,
+        Weekday.THURSDAY,
+        Weekday.FRIDAY,
+        Weekday.SATURDAY,
+        Weekday.SUNDAY
+      ])
+    })
+  })
+  describe("Enum: flags", () => {
+    class Mode extends Enum {
+      static USER_R
+      static USER_W
+      static USER_X
+      static GROUP_R
+      static GROUP_W
+      static GROUP_X
+      static ALL_R
+      static ALL_W
+      static ALL_X
+    }
+    Mode.initEnum({
+      USER_R: 0b100000000,
+      USER_W: 0b010000000,
+      USER_X: 0b001000000,
+      GROUP_R: 0b000100000,
+      GROUP_W: 0b000010000,
+      GROUP_X: 0b000001000,
+      ALL_R: 0b000000100,
+      ALL_W: 0b000000010,
+      ALL_X: 0b000000001
+    })
+    it("Using the flags", () => {
+      expect(
+        Mode.USER_R |
+          Mode.USER_W |
+          Mode.USER_X |
+          Mode.GROUP_R |
+          Mode.GROUP_X |
+          Mode.ALL_R |
+          Mode.ALL_X
+      ).toEqual(0o755)
+      expect(Mode.USER_R | Mode.USER_W | Mode.USER_X | Mode.GROUP_R).toEqual(
+        0o740
+      )
+    })
+    it("Should be type of Enum and Mode", () => {
+      expect(Mode.USER_R).toBeInstanceOf(Enum)
+      expect(Mode.USER_R).toBeInstanceOf(Mode)
+      expect(Mode.ALL_X).toBeInstanceOf(Enum)
+      expect(Mode.ALL_X).toBeInstanceOf(Mode)
+    })
   })
 })
